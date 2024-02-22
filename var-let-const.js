1d80322a7f5dbc2ba43531c9c5d793a236353d6d
arfanazmol <arfanazmol66@gmail.com>
2024-02-22 13:49:33 -0500

    practice var let const

---
+++ var-let-const.js
@@ -0,0 +1,30 @@
+// var : no reason to use var
+// let: allow it to reassign
+// const: do not allow it to reassing
+
+/*1---------let------------*/
+// let count = 30;
+// count = count + 500;
+// console.log(count)
+//------------------------//
+// let document = 150;
+// let score = 100;
+// let run = 70;
+// total = document + score * run
+// console.log(total)
+//----------------------------//
+
+/*2---------const----------------*/
+// const exam = 500;
+// const math = 500;
+// const english = 900;
+//  total = exam + math + english
+// console.log(total)
+//-------------------------------//
+
+/*2---------const----------------*/
+
+const numbers = [23, 45, 54, 76, 98];
+numbers [1] = 500;
+numbers.push(121,443,546)
+console.log(numbers);
